d89f29659c100d0835dc8cf9037422d23d712ddc
Simon Okbagergis Manna <32343117+saimenogm@users.noreply.github.com>
2025-03-18 15:16:39 +0300

    Initial-Project4

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,23 @@
 import Sidebar from "../components/dashboard/Sidebar";
 import Navbar from "../components/dashboard/Navbar";
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
       <Sidebar />
 
-      {/* Main Content Area */}
-      <div className="flex flex-col flex-1 overflow-hidden">
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* Navbar */}
         <Navbar />
-        <main className="flex-1 overflow-y-auto p-6">{children}</main>
+
+        {/* Page Content */}
+        <main className="flex-1 overflow-y-auto p-4">{children}</main>
       </div>
     </div>
   );
